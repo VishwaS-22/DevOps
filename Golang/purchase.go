bb6f32ecd804eb817cbb88e622003d430ab8162e
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-05-27 16:21:55 +0530

    Conditional If & Comparison

--- Golang/purchase.go
+++ Golang/purchase.go
@@ -28,7 +28,6 @@ the age of the vehicle as arguments and returns the estimated price in the deale
 package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
-
 func NeedsLicense(kind string)  bool {
 	//panic("NeedsLicense not implemented")
     if kind=="car"||kind =="truck"{
@@ -39,14 +38,11 @@ func NeedsLicense(kind string)  bool {
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
-
 func ChooseVehicle(option1, option2 string) string {
     if option1<option2{
         return option1+" is clearly the better choice."
     }
-    return option2+" is clearly the better choice."
-    
-    
+    return option2+" is clearly the better choice."  
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.

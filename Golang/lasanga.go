1e90a97ed52ec70c9d92eb9d1d40680c09d001c7
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-05-27 16:20:18 +0530

    Arithmetic Operations

--- Golang/lasanga.go
+++ Golang/lasanga.go
@@ -1,4 +1,5 @@
-/*Instructions
+//Instructions
+/*
 In this exercise you're going to write some code to help you cook a brilliant lasagna from your favorite cooking book.
 
 You have four tasks, all related to the time spent cooking the lasagna.

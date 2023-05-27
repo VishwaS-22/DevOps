b795addb2d138a08c232f2a551cd890b2ca13fde
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-05-27 15:52:02 +0530

    Comments

--- Golang/weather.go
+++ Golang/weather.go
@@ -12,19 +12,15 @@ You feel obligated to clarify the program so goblins can understand them as well
 */
 
 //Package weather forcasts about the weather condition.
-
 package weather
 
 //CurrentCondition will said about current weather.
-
 var CurrentCondition string
 
 //CurrentLocation will said about current location.
-
 var CurrentLocation string
 
 //Forecast will said about current weather at current at current location.
-
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition

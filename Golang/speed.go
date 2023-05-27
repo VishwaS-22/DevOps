c30a43c72eee836a738277278368743d558878a4
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-05-27 16:23:51 +0530

    Struct Type

--- Golang/speed.go
+++ Golang/speed.go
@@ -33,7 +33,6 @@ might not necessarily be fully charged when starting the race
 package speed
 
 // TODO: define the 'Car' type struct
-
 type Car struct{
     battery int
     batteryDrain int
@@ -42,7 +41,6 @@ type Car struct{
 }
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
-
 func NewCar(speed, batteryDrain int) Car {
 	return Car{
         battery: 100,
@@ -52,13 +50,11 @@ func NewCar(speed, batteryDrain int) Car {
 }
 
 // TODO: define the 'Track' type struct
-
 type Track struct{
     distance int
 } 
 
 // NewTrack creates a new track
-
 func NewTrack(distance int) Track {
 	return Track{
         distance: distance,
@@ -66,7 +62,6 @@ func NewTrack(distance int) Track {
 }
 
 // Drive drives the car one time. If there is not enough battery to drive one more time, the car will not move.
-
 func Drive(car Car) Car {
 	if newbattery:=car.battery-car.batteryDrain;newbattery>=0{
         car.battery=newbattery
@@ -76,7 +71,6 @@ func Drive(car Car) Car {
 }
 
 // CanFinish checks if a car is able to finish a certain track.
-
 func CanFinish(car Car, track Track) bool {
 	return car.battery/car.batteryDrain*car.speed>=track.distance
 }

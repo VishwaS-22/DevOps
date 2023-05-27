ee835e6b8bdb4798894cff5fe0cfff60494e6b49
Vishwa S <85573504+VishwaS-22@users.noreply.github.com>
2023-05-27 16:03:06 +0530

    Strings & Its Packages

--- Golang/techpalace.go
+++ Golang/techpalace.go
@@ -16,20 +16,17 @@ package techpalace
 import "strings"
 
 // WelcomeMessage returns a welcome message for the customer.
-
 func WelcomeMessage(customer string) string {
 	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
-
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
 	a:= strings.Repeat("*", numStarsPerLine)
 	return a+ "\n" + welcomeMsg + "\n" +a
 }
 
 // CleanupMessage cleans up an old marketing message.
-
 func CleanupMessage(oldMsg string) string {
 	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
 }
